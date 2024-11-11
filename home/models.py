d594aada88848634f43d064f442ec71d7f28a7b4
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2024-11-11 16:25:05 +0100

    fix cv upload file

--- home/models.py
+++ home/models.py
@@ -158,7 +158,7 @@ class Candidature(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
     job_title = models.CharField(max_length=100)
-    cv = models.FileField(upload_to="cvs/")
+    cv = models.FileField(upload_to="cvs/", null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
