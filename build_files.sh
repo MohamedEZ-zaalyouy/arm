d1717da0250aae5138060eb48c3544e5dd96d599
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2024-11-11 14:21:30 +0100

    add venv

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,14 @@
-echo "BUILD START"
+echo "BUILD START add VENV"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.10 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
 
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
 
 echo "BUILD END"
